eb93d9a775ee02d3bd10ff686d93e5c3e069acf0
Ruben Bulgheroni <ruben.bulgheroni@ceff.ch>
2022-12-20 16:23:12 +0100

    Choco

--- ProjetChoco.ps1
+++ ProjetChoco.ps1
@@ -6,7 +6,7 @@ Write-Host "Option (1) pour installer Chocolatey" -ForegroundColor Cyan
 Write-Host "Option (2) pour mettre à jour tous ces logiciels" -ForegroundColor Yellow
 Write-Host "Option (3) Pour installer une préconfiguration" -ForegroundColor Green
 Write-Host "Option (4) Pour installer un logiciel sur un aurtre pc" -ForegroundColor Magenta
-# Fait une popup qui demande a l'utilisateur de lancer kle script ou non 
+# Fait une popup qui demande a l'utilisateur de lancer le script ou non 
 .\popup.ps1 -Message "Voulez-vous lancer mon script ?"
 # Demande à l'utilisateur de choisir une option
 $response = Read-Host "Entrez le numéro de l'option"
@@ -15,12 +15,12 @@ switch ($response){
     1 { Powershell.exe -NoExit .\InstallChoco.ps1 }
     2 { choco upgrade all }
     3 { 
-        New-Item -Path "C:\Program Files" -name "Config1" -ItemType directory -force
+        New-Item -Path "C:\Programmes (x86)" -name "Config1" -ItemType directory -force
         choco install firefox --force -y -d "Config1"
-        choco install 7zip --force -y -d "C:\Program Files\Config1"
-        choco install googlechrome --force -y -d "C:\Program Files\Config1"
-        choco install jre8 --force -y -d "C:\Program Files\Config1"
-        choco install notepadplusplus.install --force -y -d "C:\Program Files\Config1"
+        choco install 7zip --force -y -d "C:\Programmes (x86)\Config1"
+        choco install googlechrome --force -y -d "C:\Programmes (x86)\Config1"
+        choco install jre8 --force -y -d "C:\Programmes (x86)\Config1"
+        choco install notepadplusplus.install --force -y -d "C:\Programmes (x86)\Config1"
     }
     4 { $AdresseIP = Read-Host "Entrez l'adresse ip de votre pc"
         $Logiciel = Read-Host "Que voulez vous installez ?"
